3bbf380f67e23cfe09d638f4d0efcb3cbdb26434
thanaphat <thanaphat_sriboonma@hcmu.ac.th>
2025-02-08 13:00:32 +0700

    Task 3

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,24 @@ if (typeof x === 'string') {
 }else {
    console.log('x is neither a string nor a number');
 }
+
+ const add = (a:number,b:number):string => {
+    const result =  a+b;
+    return result.toString();
+ }
+
+ function findMax(numbers: number[]): string {
+    if (numbers.length === 0) {
+        return "List is empty";
+    }
+    let maxNumber = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > maxNumber) {
+            maxNumber = numbers[i];
+        }
+    }
+    return maxNumber.toString();
+}
+// Example usage
+const numbers: number[] = [1, 2, 3, 4, 5];
+console.log(`The maximum value is: ${findMax(numbers)}`);
