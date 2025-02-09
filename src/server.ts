eb6f364cee6a6f548438c2c8e33a952e0fc59805
thanaphat <thanaphat_sriboonma@hcmu.ac.th>
2025-02-09 12:02:24 +0700

    Task 8

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
+
+app.get('/test', (req, res) => {
+    let returnObj = {
+        name: 'test',
+        age: 20,
+        address: 'Thai'
+    }
+    res.send(returnObj);
+})
+
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;   
+    const output = `id: ${id}`;
+    res.send(output);
+  })
